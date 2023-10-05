6a2460997d6205af2c39f374ed76158700bf2e42
MGDCl <sanginarioxd@gmail.com>
2023-10-05 12:22:24 -0500

    added target to /stats <player>

--- src/main/java/io/github/Leonardo0013YT/UltraCTW/cmds/StatsCMD.java
+++ src/main/java/io/github/Leonardo0013YT/UltraCTW/cmds/StatsCMD.java
@@ -1,6 +1,7 @@
 package io.github.Leonardo0013YT.UltraCTW.cmds;
 
 import io.github.Leonardo0013YT.UltraCTW.UltraCTW;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,11 +12,21 @@ public class StatsCMD implements CommandExecutor {
     public StatsCMD(UltraCTW plugin) {
         this.plugin = plugin;
     }
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (sender instanceof Player) {
+        if(sender instanceof Player) {
             Player p = (Player) sender;
-            p.sendMessage(plugin.getLang().get(p, "stats"));
+            if(args.length != 1) {
+                p.sendMessage(plugin.getLang().get(p, "stats"));
+                return true;
+            }
+            if(Bukkit.getPlayer(args[0]) == null) {
+                p.sendMessage(plugin.getLang().get("messages.notOnline"));
+                return true;
+            }
+            Player target = Bukkit.getPlayer(args[0]);
+            p.sendMessage(plugin.getLang().get(target, "stats"));
         }
         return false;
     }
